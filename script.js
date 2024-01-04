c7f4b5a9babcc051506efc1f62a8f5670b22a713
dnurdwi <dnur52250@gmail.com>
2024-01-04 09:25:30 +0700

    first commit

---
+++ script.js
@@ -0,0 +1,20 @@
+// insert angka ke textview
+function insert(num){
+    document.form.textarea.value = document.form.textarea.value + num;
+}
+
+// Funggsi Hitung
+function equal(){
+    var hasil = document.form.textarea.value;
+    document.form.textarea.value = eval(hasil);
+}
+
+//Clean
+function clean(){
+    document.form.textarea.value = "";
+}
+//Delete
+function back(){
+    var hasil = document.form.textarea.value;
+    document.form.textarea.value = hasil.substring(0,hasil.length-1);
+}
